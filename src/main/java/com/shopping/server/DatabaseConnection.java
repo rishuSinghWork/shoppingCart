93743380a93bbaa3eeb06cc710bff21d39e1895f
rishuSinghWork <rishusingh24.work@gmail.com>
2024-12-20 14:44:55 +0530

    DB conn cred change

--- src/main/java/com/shopping/server/DatabaseConnection.java
+++ src/main/java/com/shopping/server/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/shopping_cart";
-	private static final String USER = "root";
-	private static final String PASSWORD = "Reeshu@24";
+	private static final String USER = "####";
+	private static final String PASSWORD = "#########";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASSWORD);
